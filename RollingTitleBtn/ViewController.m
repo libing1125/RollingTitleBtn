dd0073811ebf89c58e19884c67fb204d04f50903
baigang <aniosdev@gmail.com>
2015-09-25 14:32:14 +0800

    添加按钮

--- RollingTitleBtn/ViewController.m
+++ RollingTitleBtn/ViewController.m
@@ -18,14 +18,12 @@
     [super viewDidLoad];
     _notice = [[RollingTitleBtn alloc]init];
     _notice.frame = CGRectMake(20, 80, self.view.bounds.size.width-40, 30);
-    NSMutableArray *ticketStrings = [NSMutableArray arrayWithObjects:@"第一行开始了",@"文件夹的菜市场时更换v", @"第二行开始了",nil];
+    NSMutableArray *ticketStrings = [NSMutableArray arrayWithObjects:@"第一句开始了",@"第二句开始了", @"恢复到我就看见回家家环境和基金会将建军节",nil];
     [_notice setTextStrings:ticketStrings];
     [_notice setRollingSpeed:40.f];
     _notice.contentLabel.textColor = [UIColor yellowColor];
     _notice.contentLabel.font = [UIFont systemFontOfSize:10];
-    if (ticketStrings.count > 0) {
-        [_notice start];
-    }
+    [_notice start];
     [_notice setTitleColor:[UIColor orangeColor] forState:UIControlStateNormal];
     _notice.titleLabel.font = [UIFont systemFontOfSize:10];
     _notice.titleLabel.textAlignment = NSTextAlignmentLeft;
@@ -39,6 +37,13 @@
 -(void)enterMessage:(id)sender
 {
     NSLog(@"Rolling");
+}
+
+- (IBAction)pause:(id)sender {
+    [_notice pause];
+}
+- (IBAction)resume:(id)sender {
+    [_notice resume];
 }
 
 - (void)didReceiveMemoryWarning {
